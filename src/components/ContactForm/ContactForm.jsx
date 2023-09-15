cf589b873002f3e97299b6680b82aa3ef47c698d
yuliatos12 <125970340+yuliatos12@users.noreply.github.com>
2023-09-15 20:47:17 +0300

    +

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
-import css from "./ContactForm.module.css"
+
 
 export class ContactForm extends Component {
     state = {
